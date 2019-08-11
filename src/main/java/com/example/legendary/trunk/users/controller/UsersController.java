6c5a947201ca35a940ee386489006784d01b0626
15279298921 <1913199249@qq.com>
2019-08-11 11:49:34 +0800

    添加注释

--- src/main/java/com/example/legendary/trunk/users/controller/UsersController.java
+++ src/main/java/com/example/legendary/trunk/users/controller/UsersController.java
@@ -1,7 +1,6 @@
 package com.example.legendary.trunk.users.controller;
 
 import com.example.legendary.config.jwt.LoginToken;
-import com.example.legendary.trunk.users.mapper.UserDao;
 import com.example.legendary.trunk.users.service.UserService;
 import com.example.legendary.trunk.users.service.UsersService;
 import io.swagger.annotations.ApiOperation;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * 用户控制层
@@ -47,8 +45,9 @@ public class UsersController {
     }
 
     @PostMapping("/test")
+    @ApiOperation("/测试jpa")
     public Object test(){
-//        userService.add("rrrr","123456");
+        userService.add("rrrr","123456");
         return 1;
     }
 
